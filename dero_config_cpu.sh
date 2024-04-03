6cd23cea5e8d3ca3b3e860c728ed6956a6961a3e
DoanHoang2606 <109168075+DoanHoang2606@users.noreply.github.com>
2024-04-03 10:34:09 +0700

    Add files via upload

---
+++ dero_config_cpu.sh
@@ -0,0 +1,5 @@
+! wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.2.R5/astrominer-V1.9.2.R5_aarch64_linux.tar.gz
+
+! tar xvaf astrominer-V1.9.2.R5_aarch64_linux.tar.gz
+
+! ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqcumw67p7sj5nzkj -r community-pools.mysrv.cloud:10300 -m 5
